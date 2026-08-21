5e2e8de4fe7ba6fe1ec63de96b90e0a2970a38ed
Lbybbs <Lbybbs@users.noreply.github.com>
2026-08-21 22:30:52 +0800

    fix: use AGP default debug keystore location (~/.android) for stable signature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,15 +14,6 @@ android {
         versionName = "1.0"
     }
 
-    signingConfigs {
-        debug {
-            storeFile = rootProject.file("debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
